2174709f2a59f62309732a13dc74cff5917185b9
ciurte-tudor-freelance <ciurte.tudor.freelance@gmail.com>
2023-08-23 10:32:26 +0000

    Added the tailwind css config.

---
+++ apps/hangman/tailwind.config.js
@@ -0,0 +1,23 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
+};
